159d795755091c4988941b344d7c6bb8c8c1da44
MazpissVisual <Mazpissiqbal@gmail.com>
2024-06-15 14:30:10 +0700

    add logoscreen

---
+++ app/src/main/java/com/mazpiss/skripsi/Logoscreen.kt
@@ -0,0 +1,23 @@
+package com.mazpiss.skripsi
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class Logoscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_logoscreen)
+
+        // Menunggu selama 2 detik sebelum pindah ke MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Menutup Logoscreen activity
+        }, 2000) // 2000 ms = 2 detik
+    }
+}
